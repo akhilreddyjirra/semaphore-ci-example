9fdef68cbee86f8ff210ab54bd21d80795ad1c65
Viraj Khatavkar <viraj.2438@gmail.com>
2017-01-05 15:07:02 +0530

    Some changes for parallel test jobs

--- tests/RegisterTest.php
+++ tests/RegisterTest.php
@@ -4,16 +4,16 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class RegisterTest extends TestCase
 {
     /**
      * A basic functional test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testRegisterPage()
     {
-        $this->visit('/')
-             ->see('Laravel');
+        $this->visit('/register')
+             ->see('Register Now');
     }
 }
